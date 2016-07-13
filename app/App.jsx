9b15bbfc1bc3e350f7e6ac74279db46e5c4ebe04
Tonida Baraza <tonida.baraza@andela.com>
2016-07-13 10:05:39 +0300

    add state to component App

--- app/App.jsx
+++ app/App.jsx
@@ -4,14 +4,56 @@ import React from 'react';
 // create a component called App
 class App extends React.Component {
   // render method returns how we want our html template to look like
+  constructor() {
+    super();
+    this.state = {
+      word: 'WORLD',
+      data:
+      [
+        {
+          word: 'world is wild and very .....',
+          reaction: 'ulala'
+        },
+        {
+          word: 'funny',
+          reaction: 'boo'
+        },
+        {
+          word: 'toni',
+          reaction:'we will see'
+        }
+
+      ]
+
+    }
+  }
+
   render () {
     return (
       <div>
-        <h1>Hello world</h1>
+        <h1>Hello {this.state.word}</h1>
+        <table>
+          <tbody>
+            {this.state.data.map((person, index) => <TableRow key = {index} data = {person} />)}
+          </tbody>
+        </table>
         <h2>Content</h2> {/*h1 and h2 are wrapped in div element */}
+        <i data-myattribute = "new">This is it</i> {/* custom attribute must have data prefix*/}
+        <h3>{23+7}</h3> {/* javascript expressions*/}
       </div>
     );
   }
+}
+
+class TableRow extends React.Component {
+   render() {
+      return (
+         <tr>
+            <td>{this.props.data.word}</td>
+            <td>{this.props.data.reaction}</td>
+         </tr>
+      );
+   }
 }
 
 // enables App.jsx to be imported
